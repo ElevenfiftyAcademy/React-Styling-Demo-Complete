555fa1bfaeece14ef74a20c67b551e53841559ce
Alecx Moritz <hanelore@live.com>
2019-01-07 15:24:46 -0500

    added material uo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>React is Here
-
-      <Button>Hello Reactstrap</Button>
-      
+      <div>
+        <h2>Hello React</h2>
       </div>
     );
   }
