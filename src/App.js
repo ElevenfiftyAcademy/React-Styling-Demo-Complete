942b11490c72f45f8f6321506802cfbf1d18a788
Alecx Moritz <hanelore@live.com>
2019-01-07 14:34:54 -0500

    added reactstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>React is Here</div>
+      <div>React is Here
+
+      <Button>Hello Reactstrap</Button>
+      
+      </div>
     );
   }
 }
